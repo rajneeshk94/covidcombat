65822b4d3159a6778bfa78ce4fe2edf42870d620
Rajneesh Khare <rajneeshkhare94@gmail.com>
2021-05-13 18:44:41 +0530

    initial version

---
+++ covidcombat/covidcombatapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name = 'home'),
+    path('register/', views.register, name = 'register'),
+    path('login/', auth_views.LoginView.as_view(template_name='covidcombatapp/login.html'), name='login'),
+]
---
+++ covidcombat/covidcombatapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
+def home(request):
+	return render(request, 'covidcombatapp/home.html')
+
+def register(request):
+	if request.method == 'POST':
+		form = UserRegisterForm(request.POST)
+		
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Your account has been created! You can now login')
+			return redirect('login')
+	else:	
+		form = UserRegisterForm()
+	return render(request, 'covidcombatapp/register.html', { 'form': form })
